13b38a3dbaadec32a5e8b60e9496c03d94246334
Chris <chris.haack@me.com>
2017-01-06 11:41:52 -0500

    add form to navigate programatically; grab Router from context and push path to it to navigate

--- lessons/12-navigating/modules/Repos.js
+++ lessons/12-navigating/modules/Repos.js
@@ -2,6 +2,24 @@ import React from 'react'
 import NavLink from './NavLink'
 
 export default React.createClass({
+
+  // ask for `router` from context
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    console.log(userName)
+    console.log(repo)
+    const path = `/repos/${userName}/${repo}`
+    console.log(path)
+    this.context.router.push(path)
+  },
+
+
   render() {
     return (
       <div>
@@ -9,6 +27,13 @@ export default React.createClass({
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/> {' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
         {this.props.children}
       </div>
